8dcec96007da7219ecb8d36a4c6c45c501cb3523
caichenyue <caichenyue@mansuo.com>
2020-05-22 08:51:51 +0800

    Updates

---
+++ precache-manifest.31d04b9b39123469ac5b52d44a158bd8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424b45ea53fd001d55894ad804e44ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590730294e18e29c97cf",
+    "url": "/static/css/2.802dbce3.chunk.css"
+  },
+  {
+    "revision": "34fcdfc353ee603ddbc5",
+    "url": "/static/css/main.e7c21831.chunk.css"
+  },
+  {
+    "revision": "590730294e18e29c97cf",
+    "url": "/static/js/2.1ec9a6dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1ec9a6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fcdfc353ee603ddbc5",
+    "url": "/static/js/main.6648c88d.chunk.js"
+  },
+  {
+    "revision": "34d1e7573984b0c083b7",
+    "url": "/static/js/runtime-main.147bef46.js"
+  },
+  {
+    "revision": "037320521d31a107bbd31f3a80a413ed",
+    "url": "/static/media/bg@2x.03732052.png"
+  },
+  {
+    "revision": "93b5c18735120b00c73b7cbcb6200090",
+    "url": "/static/media/biaoti@2x.93b5c187.png"
+  },
+  {
+    "revision": "4acf7ddae9012d46f371f0e0493d4b34",
+    "url": "/static/media/biaoti_leixing@2x.4acf7dda.png"
+  },
+  {
+    "revision": "cd67ee384f867522e83642b97280dd3b",
+    "url": "/static/media/gailan@2x.cd67ee38.png"
+  }
+]);
